09c1740a0ffeb664ffeb0cf5eda3842f4bd833db
n2one <kubawojtasik70@gmail.com>
2019-12-31 17:19:42 +0100

    display date

--- src/app/logged-layout/logged-layout.component.ts
+++ src/app/logged-layout/logged-layout.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {BasicInfoService} from '../basic-info.service';
 
 @Component({
   selector: 'app-logged-layout',
@@ -6,11 +7,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./logged-layout.component.css']
 })
 export class LoggedLayoutComponent implements OnInit {
+  date;
+  loadDate;
 
-  constructor() {
+  constructor(private basicInfoService: BasicInfoService) {
+    this.date = this.basicInfoService.getDate();
+    this.loadDate = setInterval(() => {
+      this.date = this.basicInfoService.getDate();
+      if (this.date != undefined) {
+        clearInterval(this.loadDate);
+      }
+    }, 500);
   }
 
   ngOnInit() {
+
   }
 
 }
