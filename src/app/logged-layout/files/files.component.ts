2cc35f31442cf9bcf31cee7f0d6bdaedb47004c3
n2one <kubawojtasik70@gmail.com>
2020-03-11 21:51:47 +0100

    add adding file System

--- src/app/logged-layout/files/files.component.ts
+++ src/app/logged-layout/files/files.component.ts
@@ -5,13 +5,39 @@ import { FileSystemService } from '../../services/file-system.service';
   selector: 'app-files',
   templateUrl: './files.component.html',
   styleUrls: ['./files.component.css'],
-  providers: [ FileSystemService ]
+  providers: [FileSystemService]
 })
 export class FilesComponent implements OnInit {
 
-  constructor() { }
+  addingFileForm: any;
 
-  ngOnInit() {
+  constructor(public fileSystemService: FileSystemService) {
   }
 
+  onFileChanged(event) {
+    this.addingFileForm.upload = event.target.files[0];
+  }
+
+  ngOnInit() {
+    this.addingFileForm = {
+      name: '',
+      description: '',
+      type: 0,
+      upload: File = null
+    };
+  }
+
+  uploadFile() {
+
+    this.fileSystemService.uploadFile(this.addingFileForm.name, this.addingFileForm.description, this.addingFileForm.type, this.addingFileForm.upload).then(() => {
+      console.log('ok');
+    }).catch(e => {
+      console.error(e);
+    });
+  }
+
+  getTypes() {
+
+    return this.fileSystemService.types();
+  }
 }
--- src/app/services/file-system.service.ts
+++ src/app/services/file-system.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import * as data from '../../config.json';
 
@@ -6,8 +6,70 @@ import * as data from '../../config.json';
   providedIn: 'root'
 })
 export class FileSystemService {
+  private dataURL;
+  private typesOfFile = new Array<TypeOfFile>();
 
-  constructor( private http: HttpClient ) {
-    
+  constructor(private http: HttpClient) {
+    this.dataURL = (data as any).default;
+    this.getTypesOfFiles();
   }
+
+  public types(): Array<TypeOfFile> {
+    return this.typesOfFile;
+  }
+
+  private getTypesOfFiles() {
+    var url = this.dataURL.server + this.dataURL.endpoints.fileSystem.getTypes;
+    const httpOption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'token ' + localStorage.getItem('token')
+      })
+    };
+    this.http.get(url, httpOption).subscribe(
+      (data: any) => {
+        console.log('OK');
+        for (var type of data) {
+          this.typesOfFile.push(new TypeOfFile(type.pk, type.name));
+        }
+        console.log(this.typesOfFile);
+      }
+    );
+  }
+
+  public uploadFile(name, description, type, file: File): Promise<any> {
+    var url = this.dataURL.server + this.dataURL.endpoints.fileSystem.addFile;
+    let formData: FormData = new FormData();
+
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('fileType', type);
+    formData.append('upload', file);
+    const httpOption = {
+      headers: new HttpHeaders({
+        'Authorization': 'token ' + localStorage.getItem('token')
+      })
+    };
+
+    return new Promise<any>((p, e) => this.http.post(url, formData, httpOption).subscribe(
+      (data: any) => {
+
+        p = data;
+      },
+      (error: any) => {
+        e = error;
+      }
+    ));
+  }
+}
+
+class TypeOfFile {
+  constructor(pk: number, name: string) {
+    this.id = pk;
+    this.name = name;
+  }
+
+  id: number;
+  name: string;
+
 }
