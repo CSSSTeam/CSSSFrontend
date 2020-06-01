68e7a8f671cce2296839ea45f3f270230843176d
mwawrzen <w.w@op.pl>
2020-06-01 14:03:37 +0200

    infos-edit layout

---
+++ src/app/logged-layout/infos/infos-edit/infos-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfosEditComponent } from './infos-edit.component';
+
+describe('InfosEditComponent', () => {
+  let component: InfosEditComponent;
+  let fixture: ComponentFixture<InfosEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InfosEditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InfosEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/logged-layout/infos/infos-edit/infos-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-infos-edit',
+  templateUrl: './infos-edit.component.html',
+  styleUrls: ['./infos-edit.component.css']
+})
+export class InfosEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
