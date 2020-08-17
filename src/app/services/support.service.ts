59d99e91d1d06694e817a594a2af085390d0d59d
n2one <kubawojtasik70@gmail.com>
2020-08-17 21:09:49 +0200

    del file

--- src/app/services/support.service.ts
+++ src/app/services/support.service.ts
@@ -1,20 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupportService {
 
-  constructor() { }
+  constructor() {
+  }
 
   imgW;
 
   scrollDown(fromTop: number, timeout: number) {
-    window.setTimeout(function () {
+    window.setTimeout(function() {
       window.scroll({
         top: fromTop,
         behavior: 'smooth'
-      })
+      });
     }, timeout);
   }
 
@@ -24,16 +25,19 @@ export class SupportService {
 
   popup(text) {
     const popup = document.createElement('div');
-    if (window.innerWidth >= 1350) this.imgW = 150;
-    else this.imgW = 80;
+    if (window.innerWidth >= 1350) {
+      this.imgW = 150;
+    } else {
+      this.imgW = 80;
+    }
     popup.classList.add('popup');
-    popup.innerHTML = '<div><img src="../assets/img/check.png" width="' + this.imgW + '"></img><br> pomyślnie <br>' + text + '</div>';
+    popup.innerHTML = '<div><img src="../assets/img/check.png" width="' + this.imgW + '"/><br> pomyślnie <br>' + text + '</div>';
     document.body.appendChild(popup);
     popup.addEventListener('click', () => document.body.removeChild(popup));
     setTimeout(() => document.body.removeChild(popup), 1500);
   }
 
-  statement(text, pop) {
+  statement(text, yesCallBack, backCallBack = (() => null)) {
     const statement = document.createElement('div');
     statement.innerHTML = '<div id="box"><p>Czy na pewno chcesz ' + text + '?</p><div><button class="yes">Tak</button><button class="back">Cofnij</button></div></div>';
 
@@ -44,9 +48,12 @@ export class SupportService {
     const back = document.querySelector('.back');
     yes.addEventListener('click', () => {
       document.body.removeChild(statement);
-      return this.popup(pop);
+      yesCallBack();
     });
-    back.addEventListener('click', () => document.body.removeChild(statement));
+    back.addEventListener('click', () => {
+      document.body.removeChild(statement);
+      backCallBack();
+    });
   }
 
 }
