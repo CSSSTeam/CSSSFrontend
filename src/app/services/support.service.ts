e7a298016dc6eebd5fcabd56bb6e7136c9581e9a
mwawrzen <w.w@op.pl>
2020-08-21 00:40:29 +0200

    added error option

--- src/app/services/support.service.ts
+++ src/app/services/support.service.ts
@@ -29,8 +29,12 @@ export class SupportService {
     else this.imgW = 80;
 
     popup.classList.add('popup');
-    if (type == 'correct' || type == 'error') {
-      popup.innerHTML = `<div><img src="../assets/img/${type}.png width="${this.imgW}"></img><br>${text}</div>`
+    if (type == 'success' || type == 'error') {
+      popup.innerHTML = `
+<div>
+  <img src="../assets/img/${type}.png" width="${this.imgW}"></img><br>${text}
+</div>
+      `;
     } else console.error("wrong type argument in popup()");
 
     document.body.appendChild(popup);
@@ -40,7 +44,16 @@ export class SupportService {
 
   statement(text, pop) {
     const statement = document.createElement('div');
-    statement.innerHTML = '<div id="box"><p>Czy na pewno chcesz ' + text + '?</p><div><button class="yes">Tak</button><button class="back">Cofnij</button></div></div>';
+    statement.innerHTML = `
+<div id="box">
+  <p>Czy na pewno chcesz ${text}?</p>
+  <div>
+    <button class="yes">Tak</button>
+    <button class="back">Cofnij</button>
+  </div>
+</div>
+    `;
+    // statement.innerHTML = '<div id="box"><p>Czy na pewno chcesz ' + text + '?</p><div><button class="yes">Tak</button><button class="back">Cofnij</button></div></div>';
 
     statement.classList.add('statement');
     document.body.appendChild(statement);
@@ -49,9 +62,11 @@ export class SupportService {
     const back = document.querySelector('.back');
     yes.addEventListener('click', () => {
       document.body.removeChild(statement);
-      this.popup(pop, 'correct');
+      this.popup(pop, 'success');
     });
-    back.addEventListener('click', () => document.body.removeChild(statement));
+    back.addEventListener('click', () => {
+      document.body.removeChild(statement)
+    });
   }
 
 }
